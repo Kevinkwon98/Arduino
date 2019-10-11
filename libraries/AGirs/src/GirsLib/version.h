f6141c8b70cc458692d199b03e9dcf148214f9a4
Kevinkwon98 <keunhyug@gmail.com>
2019-10-11 21:47:59 +0900

    all files ware deleted

--- libraries/AGirs/src/GirsLib/version.h
+++
@@ -1,6 +0,0 @@
-// This file was automatically generated from library.properties; do not edit.
-/**
- * Version of the current library.
- * Taken from the version in library.properties.
- */
-#define VERSION "1.0.2"
